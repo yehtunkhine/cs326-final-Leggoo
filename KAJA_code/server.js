3123911c3be49d6dcf460e55bdc8152283176031
Ye Tun <yehtunkhine@gmail.com>
2022-04-20 19:45:20 -0400

    fixed hobbies list in profile.html && linked all html pages together through href

--- KAJA_code/server.js
+++ KAJA_code/server.js
@@ -5,37 +5,13 @@ import { readFile, writeFile } from 'fs/promises';
 
 const app = express();
 const port = 3000;
-// const path = require('path');
-const path = '/Users/khai/Desktop/cs326/kaja/milestone2/cs326-final-Leggoo/KAJA_code/';
 
 app.use(logger('dev'));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-// app.use('/static', express.static('public'));
-
-// connect to homepage html
-app.get('/', (req, res) => {
-    res.sendFile(path + '/public/homePage.html');
-});
-
-// connect to login page html
-app.get('/login', (req, res) => {
-    res.sendFile(path +'/public/signIn.html');
-    
-});
-
-// connect to profile page html
-app.get('/profile', (req, res) => {
-    res.sendFile(path + '/public/profile.html');
-});
-
-app.get('/register', (req, res) => {
-    res.sendFile(path + '/public/Register.html');   
-});
-
-
+app.use('/static', express.static('public'));
 
 //const datafile = 'Data-Test.json';
 
@@ -49,7 +25,7 @@ const events = {};
 // Create a new user with id and name
 app.post('/create/user/:id/:name', (req, res) => {
     const user = req.params;
-    users[user.id]= user;
+    users[user.id] = user;
     // Status 201: Created
     res.status(201).json(user);
 });
@@ -65,22 +41,18 @@ app.get('/user/:id', (req, res)=> {
     }
 });
 
-// Delete a user profile with their id
-app.delete('/delete/user/:id', (req, res) => {
-    const {id} = req.params;
-    const user = users[id];
-    if(user){
-        delete users[user];
-    }
-    else{
-        res.status(404).json({message: 'User ${id} not found to delete.'});
-    }
-});
-
 // getting all users
 app.get('*', (req, res) => {
     res.status(404).json({message: 'Unknown Request'});
 });
+
+//Get user profile
+
+//Post user info changes
+
+//Delete user
+
+//
 
 // Create a new event with id, date, time, capacity and category
 app.post('/create/event/:id/:date/:time/:capacity/:category', (req, res) => {
@@ -100,9 +72,14 @@ app.get("/event/:id", (req, res) => {
     }
 });
 
+//Get all events
 
+//Post event updates
+
+//Delete events
+
+//
 
 app.listen(port, () => {
     console.log("Application running on port " + port);
-// /usr/bin/bash: q: command not found
 });
